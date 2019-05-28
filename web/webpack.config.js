c4e40c384973af786f424d4d73a488f6985c17e1
Noskcaj <kcajdev@gmail.com>
2019-05-28 06:55:04 -0400

    Move web code to src subdir

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -3,14 +3,14 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const path = require("path")
 
 module.exports = {
-    entry: "./bootstrap.js",
+    entry: "./src/bootstrap.js",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bootstrap.js"
     },
     mode: "development",
     plugins: [
-        new CopyWebpackPlugin(["index.html"]),
+        new CopyWebpackPlugin(["src/index.html"]),
         new MiniCssExtractPlugin({
             // Options similar to the same options in webpackOptions.output
             // both options are optional
